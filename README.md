768747ce6c48545d342cbdb8b975d9d540ae12c6
Víctor Araya <victor.d.araya@gmail.com>
2023-07-12 02:39:27 -0400

    se agrega logica para buscar anime por id y nombre, crear nuevo anime

--- README.md
+++ README.md
@@ -8,11 +8,11 @@ Dentro del marco de lo aprendido durante este Drill (modulo), se debe desarrolla
 
 ### Requerimientos
 
--   Crear un servidor con node/express.
--   Crear un archivo principal llamado index.js.
--   En un archivo aparte, llamado anime.json, guardar los datos con la siguiente información: [anime.json](./anime.json)
+-   Crear un servidor con node/express. ✔
+-   Crear un archivo principal llamado index.js. ✔
+-   En un archivo aparte, llamado anime.json, guardar los datos con la siguiente información: [anime.json](./anime.json) ✔
 -   Crear un programa que permita hacer el CRUD completo de los datos. El id será el primer argumento para acceder a las propiedades de cada anime.
--   Se deberá poder listar todos los datos del archivo y, además, leer los datos de un anime especifico, accediendo por su id y / o por su nombre.
+-   Se deberá poder listar todos los datos del archivo y, además, leer los datos de un anime especifico, accediendo por su id y / o por su nombre. ✔
 -   Realizar un test para poder probar la respuesta del servidor que fue creado.
 
 ### Instalación / Ejecución
@@ -33,6 +33,44 @@ Ejecución de testing:
 
 ```
 npm test
+```
+
+### End Points
+
+Listar Todo
+
+```
+http://localhost:3000/
+```
+
+Añadir nuevo Anime:
+
+```
+http://localhost:3000/create?name=Rurouni%20Kenshin&gender=Shonen&year=1996&author=Nobuhiro%20Watsuki
+```
+
+Actualizar Anime:
+
+```
+
+```
+
+Eliminar Anime:
+
+```
+
+```
+
+Busqueda por ID:
+
+```
+http://localhost:3000/search/id/4
+```
+
+Busqueda por Nombre:
+
+```
+http://localhost:3000/search/name/neon%20genesis%20evangelion
 ```
 
 ---
--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use('/bootstrap', express.static(__dirname + '/node_modules/bootstrap/dist')
 
 // Middlewares:
 
-// Routes:
+// Rutas:
 // Muestra todo
 app.get('/', async (req, res) => {
 	try {
@@ -31,6 +31,67 @@ app.get('/', async (req, res) => {
 		res.status(201).render('home', { anime });
 	} catch (error) {
 		console.log(error);
+		res.status(500).send(error.message);
+	}
+});
+
+// Agregar Anime
+app.get('/create', async (req, res) => {
+	try {
+		const animeFile = JSON.parse(await fs.readFile(__dirname + '/anime.json'));
+		const id = Object.keys(animeFile).length + 1;
+		const nombre = req.query.name;
+		const genero = req.query.gender;
+		const anio = req.query.year;
+		const autor = req.query.author;
+
+		const newAnime = {
+			nombre,
+			genero,
+			anio,
+			autor,
+		};
+
+		animeFile[id] = newAnime;
+		await fs.writeFile(__dirname + '/anime.json', JSON.stringify(animeFile, null, 3));
+		res.status(201).render('add', { anime: [newAnime] });
+	} catch (error) {
+		res.status(500).send(error.message);
+	}
+});
+
+// Busqueda por ID.
+app.get('/search/id/:id', async (req, res) => {
+	const id = req.params.id;
+	try {
+		const animeFile = JSON.parse(await fs.readFile(__dirname + '/anime.json'));
+		const anime = animeFile[id];
+		// console.log(anime);
+		if (anime) {
+			res.status(200).render('home', { anime: [anime] });
+		} else {
+			res.status(404).send('Anime no encontrado');
+		}
+	} catch (error) {
+		console.log(error);
+		res.status(500).send(error.message);
+	}
+});
+
+// Busqueda por nombre.
+app.get('/search/name/:name', async (req, res) => {
+	const name = req.params.name.toLowerCase();
+	try {
+		const animeFile = JSON.parse(await fs.readFile(__dirname + '/anime.json'));
+		const anime = Object.values(animeFile).filter((anime) =>
+			anime.nombre.toLowerCase().includes(name)
+		);
+		if (anime.length > 0) {
+			res.status(200).render('home', { anime });
+		} else {
+			res.status(404).send('No se encontraron animes');
+		}
+	} catch (error) {
 		res.status(500).send(error.message);
 	}
 });
